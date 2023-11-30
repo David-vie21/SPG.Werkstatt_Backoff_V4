30263500c08aab64b91ba68ab4c2ae7213d93145
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-11-30 12:55:45 +0100

    seed for MongoDB Models

--- SPG.Werkstatt.Domian/ContextMongo.cs
+++ SPG.Werkstatt.Domian/ContextMongo.cs
@@ -1,9 +1,11 @@
 ﻿using Bogus;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using SPG.Werkstatt.Domian.Model;
 using SPG.Werkstatt.Domian.MongoModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SPG.Werkstatt.Domian
 {
@@ -14,7 +16,9 @@ namespace SPG.Werkstatt.Domian
         // MongoDB Kollektionen
         private IMongoCollection<CustomerMongo> _customerCollection;
         private IMongoCollection<CarMongo> _carCollection;
-        private IMongoCollection<Termin> _termineCollection;
+        private IMongoCollection<TerminMongo> _termineCollection;
+        private IMongoCollection<Day> _dayCollection;
+
 
         // Konstruktor
         public WerkstattMongoContext(IMongoDatabase database)
@@ -28,33 +32,90 @@ namespace SPG.Werkstatt.Domian
         {
             _customerCollection = _database.GetCollection<CustomerMongo>("Customers");
             _carCollection = _database.GetCollection<CarMongo>("Cars");
-            _termineCollection = _database.GetCollection<Termin>("Termine");
+            _termineCollection = _database.GetCollection<TerminMongo>("Termine");
+            _dayCollection = _database.GetCollection<Day>("Day");
+
         }
 
         // Methode zum Hinzufügen von Testdaten in MongoDB
         public void Seed()
         {
             Randomizer.Seed = new Random(1017);
-
             List<CustomerMongo> customers = new Faker<CustomerMongo>("de")
-                .Rules((f, c) =>
-                {
-                    // Hier bleibt deine Logik für das Generieren von Kundenobjekten unverändert
-                    // ...
+               .Rules((f, c) =>
+               {                    
+                   c.guid = f.Random.Guid();
+                   c.Vorname = f.Name.FirstName(Bogus.DataSets.Name.Gender.Female);
+                   c.Nachname = f.Name.LastName();
+                   c.Email = f.Internet.Email();
+                   c.Tel = f.Person.Phone;
+                   c.Addrese = f.Address.FullAddress();
 
-                })
-                .Generate(80);
-
+               })
+           .Generate(10)
+           .ToList();
             _customerCollection.InsertMany(customers);
 
-            // Ähnlich kannst du für Autos und Termine vorgehen
-            // Beachte, dass dies ein Beispiel ist und du deine eigenen Logiken für das Generieren und Hinzufügen der Daten implementieren musst.
+            List<CarMongo> cars = new Faker<CarMongo>("de")
+                .Rules((f, c) =>
+                {
+                    c.guid = f.Random.Guid();
+                    c.Besitzer = customers[f.Random.Number(0, 79)];
+                    c.Marke = f.Company.CompanyName();
+                    c.Modell = f.Commerce.ProductName();
+                    c.Kennzeichen = f.Random.Replace("W##-###");
+                    c.Erstzulassung = f.Date.Past(10, DateTime.Now);
+                    c.Kw = f.Random.Decimal(60, 500);
 
-            // List<CarMongo> cars = ... // Generierung der Autodaten
-            // _carCollection.InsertMany(cars);
+                })
+                .Generate(10);
+            _carCollection.InsertMany(cars);
 
-            // List<Termin> termine = ... // Generierung der Termin-Daten
-            // _termineCollection.InsertMany(termine);
+            List<TerminMongo> termine = new Faker<TerminMongo>().Rules((f, s) =>
+            {
+                //         public Customer Kunde { get; set; }
+                //public DateTime Datetime { get; set; }
+                //public Car Auto { get; set; }
+                //public string Summery { get; set; }
+                //public string Date { get { return Datetime.ToShortDateString(); } }
+                //public string Time { get { return Datetime.TimeOfDay.ToString(); } }
+                //public bool accepted { get; set; }
+                //public bool IsDone { get; set; }
+
+                s.guid = f.Random.Guid();
+                s.Kunde = customers[f.Random.Number(0, 79)];
+                s.Datetime = f.Date.Between(new DateTime(2023, 12, 1), new DateTime(2023, 12, 10));
+                s.Auto = cars[f.Random.Number(1, 299)];
+                s.Summery = f.Lorem.Sentence();
+                s.accepted = f.Random.Bool();
+                s.IsDone = f.Random.Bool();
+            })
+            .Generate(20)
+            .ToList();
+            _termineCollection.InsertMany(termine);
+
+
+
+            List<Day> days = new Faker<Day>("de")
+                .Rules((f, d) =>
+                {
+                    d.Date = new DateOnly(2023,12,f.Random.Number(1,10));
+                    d.Termine = termine.Where(t => new DateOnly(d.Date.Year, d.Date.Month, d.Date.Day) == new DateOnly(t.Datetime.Year, t.Datetime.Month, t.Datetime.Day)).ToList();
+                })
+                .Generate(10);
+            _dayCollection.InsertMany(days);
+
+            //var testTermine = termine.ToList();
+            //foreach (Day day in days)
+            //{
+            //    for (int i = 1; i <= 2; i++)
+            //    {
+            //        Random random = new Random();
+            //        var termin = testTermine.OrderBy(x => random.Next()).Take(2).ToList();
+
+            //        day.Termine.Add()
+            //    }
+            //}
         }
     }
 }

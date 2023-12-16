a934a17d347741fdca5eba36c8b1797ba1a52ebb
David-vie21 <david.ankenbrand98@gmail.com>
2023-12-16 20:20:01 +0100

    Find Speed Test

--- Context_Test/UnitTest1.cs
+++ Context_Test/UnitTest1.cs
@@ -5,6 +5,7 @@ using SPG.Werkstatt.Domian;
 using SPG.Werkstatt.Domian.Model;
 using System.IO;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Context_Test
 {
@@ -69,6 +70,65 @@ namespace Context_Test
                ts.Hours, ts.Minutes, ts.Seconds,
                ts.Milliseconds / 10, ts.Milliseconds);
             LogWriter.LogWrite("SQL: SeedTime x" + multi + " (hh:mm:ss:ms:ns):" + elapsedTime);
+        }
+
+        [Fact]
+        public void SpeedTest_Reading() 
+        {
+            DbContextOptions options = new DbContextOptionsBuilder()
+                    //.UseSqlite("Data Source= Werkstatt.db")
+                    .UseSqlite("Data Source= I:\\Dokumente 4TB\\HTL\\5 Klasse\\DBI\\Projekt\\SPG.Werkstatt_Backoff_V4\\SPG.Werkstatt_Backoff_V3\\Werkstatt.db")
+                    .Options;
+
+            WerkstattContext context = new WerkstattContext(options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.Seed(100);
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan ts;
+            string elapsedTime = "";
+
+            LogWriter.LogWrite("SQL Read:");
+
+
+            //x100 - Termin
+            stopwatch.Start();
+
+            var day = context.Termine.ToList().Where(t => t.Datetime.Date == new DateTime(2023, 12, 1));
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                              ts.Hours, ts.Minutes, ts.Seconds,
+                                            ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Find Termin (hh:mm:ss:ms:ns):" + elapsedTime);
+
+
+            //x100 - Customer
+            stopwatch.Start();
+
+            var customer = context.Customers.ToList().Where(d => d.Name.ToLower().Equals("david"));
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                              ts.Hours, ts.Minutes, ts.Seconds,
+                                            ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Find Customer (hh:mm:ss:ms:ns):" + elapsedTime);
+
+            //x100 - Car
+            stopwatch.Start();
+
+            var car = context.Cars.ToList().Where(d => d.Besitzer.Equals(customer));
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                              ts.Hours, ts.Minutes, ts.Seconds,
+                                            ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Find Car (hh:mm:ss:ms:ns):" + elapsedTime);
         }
     }
 

e6819f0de76c1b5fb0c310638e4d14ae12e71a75
David-vie21 <david.ankenbrand98@gmail.com>
2023-12-19 17:39:47 +0100

    Update - Delete Speed Test

--- Context_Test/UnitTest1.cs
+++ Context_Test/UnitTest1.cs
@@ -130,6 +130,159 @@ namespace Context_Test
                                             ts.Milliseconds / 10, ts.Milliseconds);
             LogWriter.LogWrite("SQL: Find Car (hh:mm:ss:ms:ns):" + elapsedTime);
         }
+
+        [Fact]
+        public void TimeTest_Update_One_SQL()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlite("Data Source= I:\\Dokumente 4TB\\HTL\\5 Klasse\\DBI\\Projekt\\SPG.Werkstatt_Backoff_V4\\SPG.Werkstatt_Backoff_V3\\Werkstatt.db")
+                .Options;
+
+            WerkstattContext context = new WerkstattContext(options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.Seed(100);
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan ts;
+            string elapsedTime = "";
+
+            LogWriter.LogWrite("SQL Update:");
+
+            // x100 - Termin Update 
+            var termin = context.Termine.FirstOrDefault();
+            termin.accepted = true;
+            termin.guid = Guid.NewGuid();
+            termin.Datetime = DateTime.Now;
+
+            stopwatch.Start();
+
+            if (termin != null)
+            {
+                context.Termine.Update(termin);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Update Termin (hh:mm:ss:ms:ns):" + elapsedTime);
+
+            // x100 - Kunden Update 
+            var customer = context.Customers.FirstOrDefault();
+            customer.guid = Guid.NewGuid();
+            customer.Addrese = "Test Add";
+            stopwatch.Start();
+
+            if (customer != null)
+            {
+                context.Customers.Update(customer);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Update Kunde (hh:mm:ss:ms:ns):" + elapsedTime);
+
+            // x100 - Auto Update 
+            var car = context.Cars.FirstOrDefault();
+            car.guid = Guid.NewGuid();
+            car.Kw = 100;
+            stopwatch.Start();
+
+            if (car != null)
+            {                
+                context.Cars.Update(car);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Update Auto (hh:mm:ss:ms:ns):" + elapsedTime);
+        }
+
+        [Fact]
+        public void TimeTest_Delete_SQL()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlite("Data Source= I:\\Dokumente 4TB\\HTL\\5 Klasse\\DBI\\Projekt\\SPG.Werkstatt_Backoff_V4\\SPG.Werkstatt_Backoff_V3\\Werkstatt.db")
+                .Options;
+
+            WerkstattContext context = new WerkstattContext(options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.Seed(100);
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan ts;
+            string elapsedTime = "";
+
+            LogWriter.LogWrite("SQL Delete:");
+
+            // x100 - Termin Delete  
+            var termin = context.Termine.FirstOrDefault();
+
+            stopwatch.Start();
+
+            if (termin != null)
+            {
+                context.Termine.Remove(termin);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Delete Termin (hh:mm:ss:ms:ns):" + elapsedTime);
+
+            // x100 - Kunden Delete 
+            var customer = context.Customers.FirstOrDefault();
+
+            stopwatch.Start();
+
+            if (customer != null)
+            {
+                context.Customers.Remove(customer);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Delete Kunde (hh:mm:ss:ms:ns):" + elapsedTime);
+
+            // x100 - Auto Delete 
+            var car = context.Cars.FirstOrDefault();
+            stopwatch.Start();
+
+            if (car != null)
+            {
+                context.Cars.Remove(car);
+                context.SaveChanges();
+            }
+
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10, ts.Milliseconds);
+            LogWriter.LogWrite("SQL: Delete Auto (hh:mm:ss:ms:ns):" + elapsedTime);
+        }
+
     }
 
     public static class LogWriter

5e5c3b3e9165a6f3c52977175b2f6988c9ef91f6
David-vie21 <david.ankenbrand98@gmail.com>
2023-12-21 16:09:33 +0100

    update Speed Methode for Aggregation

--- SPG.Werkstatt.Domian/ContextMongo.cs
+++ SPG.Werkstatt.Domian/ContextMongo.cs
@@ -127,5 +127,85 @@ namespace SPG.Werkstatt.Domian
             //    }
             //}
         }
+
+        public void Seed_with_Aggregation(int multi)
+        {
+            Randomizer.Seed = new Random(1017);
+            List<CustomerMongo> customers = new Faker<CustomerMongo>("de")
+               .Rules((f, c) =>
+               {
+                   c.guid = f.Random.Guid();
+                   c.Vorname = f.Name.FirstName(Bogus.DataSets.Name.Gender.Female);
+                   c.Nachname = f.Name.LastName();
+                   c.Email = f.Internet.Email();
+                   c.Tel = f.Person.Phone;
+                   c.Addrese = f.Address.FullAddress();
+
+               })
+           .Generate(100 * multi)
+           .ToList();
+            _customerCollection.InsertMany(customers);
+
+            List<CarMongo> cars = new Faker<CarMongo>("de")
+                .Rules((f, c) =>
+                {
+                    c.guid = f.Random.Guid();
+                    c.Besitzer = customers[f.Random.Number(0, customers.Count() - 1)];
+                    c.Marke = f.Company.CompanyName();
+                    c.Modell = f.Commerce.ProductName();
+                    c.Kennzeichen = f.Random.Replace("W##-###");
+                    c.Erstzulassung = f.Date.Past(10, DateTime.Now);
+                    c.Kw = f.Random.Decimal(60, 500);
+
+                })
+                .Generate(10 * multi);
+            _carCollection.InsertMany(cars);
+
+            List<TerminMongo> termine = new Faker<TerminMongo>().Rules((f, s) =>
+            {
+                //         public Customer Kunde { get; set; }
+                //public DateTime Datetime { get; set; }
+                //public Car Auto { get; set; }
+                //public string Summery { get; set; }
+                //public string Date { get { return Datetime.ToShortDateString(); } }
+                //public string Time { get { return Datetime.TimeOfDay.ToString(); } }
+                //public bool accepted { get; set; }
+                //public bool IsDone { get; set; }
+
+                s.guid = f.Random.Guid();
+                s.Kunde = customers[f.Random.Number(0, customers.Count() - 1)];
+                s.Datetime = f.Date.Between(new DateTime(2023, 12, 1), new DateTime(2023, 12, 10).AddDays(multi));
+                s.Auto = cars[f.Random.Number(0, cars.Count() - 1)];
+                s.Summery = f.Lorem.Sentence();
+                s.accepted = f.Random.Bool();
+                s.IsDone = f.Random.Bool();
+            })
+            .Generate(20 * multi)
+            .ToList();
+            _termineCollection.InsertMany(termine);
+
+
+
+            List<Day> days = new Faker<Day>("de")
+                .Rules((f, d) =>
+                {
+                    d.Date = new DateOnly(2023, 12, 1).AddDays(f.Random.Number(1, 1 * multi));
+                    d.Termine = termine.Where(t => new DateOnly(d.Date.Year, d.Date.Month, d.Date.Day) == new DateOnly(t.Datetime.Year, t.Datetime.Month, t.Datetime.Day)).ToList();
+                })
+                .Generate(10 * multi);
+            _dayCollection.InsertMany(days);
+
+            //var testTermine = termine.ToList();
+            //foreach (Day day in days)
+            //{
+            //    for (int i = 1; i <= 2; i++)
+            //    {
+            //        Random random = new Random();
+            //        var termin = testTermine.OrderBy(x => random.Next()).Take(2).ToList();
+
+            //        day.Termine.Add()
+            //    }
+            //}
+        }
     }
 }
